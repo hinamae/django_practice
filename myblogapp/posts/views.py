fd84147537a171666f1434939dd021549666c7cc
hina.maeyama <hina.maeyama@add-more.co.jp>
2020-07-17 08:46:26 +0900

    Postおぶじぇくとを詳細画面に返すことができるようになったことで、詳細を表示できた

--- myblogapp/posts/views.py
+++ myblogapp/posts/views.py
@@ -12,5 +12,9 @@ def index(request):
     return render(request, 'posts/index.html', {'posts':posts})
 
 def post_detail(request,post_id):
-    return render(request,'posts/post_detail.html',{'post_id':post_id})
+    # pk=プライマリキー（インデックスの番号）
+    #getメソッドで指定した番号のデータを取り出してきて、ポストのオブジェクトを渡す
+    post = Post.objects.get(pk=post_id)
+    #request,返すパス,返すデータ
+    return render(request,'posts/post_detail.html',{'post':post})
 
